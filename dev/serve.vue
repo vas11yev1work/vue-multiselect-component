7f52ca7456899317d55ccc24e5d51a3bc63646b6
Анатолий Васильев <vas11yev.work@ya.ru>
2021-05-27 10:54:37 +0300

    rename

---
+++ dev/serve.vue
@@ -0,0 +1,45 @@
+<script>
+import Vue from 'vue';
+import VueMultiselectComponent from '../src/vue-multiselect-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueMultiselectComponent
+  },
+  data() {
+    return {
+      countries: [
+        { id: 'ru', label: 'Russia' },
+        { id: 'nl', label: 'Netherlands' },
+        { id: 'fr', label: 'France' }
+      ],
+      selected: []
+    }
+  },
+  methods: {
+    test(text, v) {
+      console.log(text, v)
+    }
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-multiselect-component
+      v-model="selected"
+      searchable
+      writable
+      placeholder="Select countries"
+      :options="countries"
+    />
+  </div>
+</template>
+
+<style>
+#app {
+  max-width: 500px;
+  padding: 30px;
+}
+</style>
